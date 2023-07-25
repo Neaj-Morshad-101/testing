4534b902c009ea8abcc4cbb2a0038ac487196deb
neaj <neaj@appscode.com>
2023-07-25 19:21:49 +0600

    added-all
    Signed-off-by: neaj <neaj@appscode.com>

--- README.md
+++ README.md
@@ -14,6 +14,12 @@ Gomega is a matcher/assertion library.
 
 Installing Ginkgo
 Ginkgo uses go modules. To add Ginkgo to your project, assuming you have a go.mod file setup, just go install it:
-
 go install github.com/onsi/ginkgo/v2/ginkgo
 go get github.com/onsi/gomega/...
+
+
+Useful commands: 
+`ginkgo bootstrap` //  add a Ginkgo suite to books pkg 
+This will generate a file named books_suite_test.go in the books directory.
+
+`ginkgo generate book` // to test book.go, generate book_test.go
--- books/books_suite_test.go
+++ books/books_suite_test.go
@@ -8,6 +8,12 @@ import (
 )
 
 func TestBooks(t *testing.T) {
+	// RegisterFailHandler(Fail) is the single line of glue code connecting Ginkgo to Gomega.
+	// If we were to avoid dot-imports this would read as gomega.RegisterFailHandler(ginkgo.Fail)
+	//- what we're doing here is telling our matcher library (Gomega) which function to call (Ginkgo's Fail) in the event a failure is detected.
 	RegisterFailHandler(Fail)
+
+	// RunSpecs() call tells Ginkgo to start the test suite, passing it the *testing.T instance and a description of the suite. You should only ever call RunSpecs once and you can let Ginkgo worry about calling *testing.T for you.
+
 	RunSpecs(t, "Books Suite")
 }
